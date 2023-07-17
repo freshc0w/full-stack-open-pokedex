f6541ea51b9ea11baa728743f3a313e6a17e241b
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-07-17 18:27:39 +1000

    Add second e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,11 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('squirtle').click()
+    cy.contains('torrent')
+    cy.contains('rain-dish')
+  })
 })
