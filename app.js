c7179e4af3ff84d51f6d85aa7f9ac4f5d5f6d0e9
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-07-17 20:49:52 +1000

    Configure render deployment workflow

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('1.0.1')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT)
