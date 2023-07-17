cadc364ebe1e388d043a1c3047d25704d015bc76
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-07-17 18:06:06 +1000

    Attempt ex 11.9

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express()
 require('dotenv').config()
 
 // Heroku dynamically sets a port
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
